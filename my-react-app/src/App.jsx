377d9fb467360e30c2903176a0fa13b2493ad5ca
Abhinav Pal <meabhi1222@gmail.com>
2025-09-19 22:20:55 +0530

    setup again

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./styles.css";
-import Footer from "./components/Footer.jsx"; // ✅ Correct import
+
 
 import { FaInstagram, FaLinkedin, FaWeixin, FaBars } from "react-icons/fa";
 function App() {
@@ -12,8 +12,8 @@ function App() {
     <div className="App">
       
       
-      {/* ✅ Footer should be outside of <h1> */}
-      <Footer />  
+   
+      
     </div>
   );
 }
