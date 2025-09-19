0251fd3617d39145684ca3100219c9a4b62e86ac
Abhinav Pal <meabhi1222@gmail.com>
2025-09-20 00:48:32 +0530

    font

--- my-react-app/tailwind.config.js
+++ my-react-app/tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens:{
+        sm: '480px',
+        md:'768px',
+        lg:'976px',
+        xl:'1440px'
+      },
+    extend: {
+      fontFamily:{
+        sans:['Josefin Sans', 'sans-serif'],
+        alata:['Alata']
+
+      }
+    },
   },
   plugins: [],
 }
