f13f1dbc7285233623544269f0e0b455b1861ec4
Abhinav Pal <meabhi1222@gmail.com>
2025-10-26 22:37:13 +0530

    navbar update

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -4,12 +4,35 @@ export default function Navbar()
     return(
         <div>
             <section id = "Navbar">
-                <div  className=" container max-w-6xl mx-auto px-5 py-10 md:px-0">
+                <div  className=" container max-w-6xl mx-auto px-5 py-10 ">
                     <nav className="flex item-center justify-between font-bold text-white">
                     <img src={logo} alt="Logo" />
+
+                    <div className="hidden h-10 font-ubuntu md:flex md:space-x-9">
+                        <div className="group">
+                          <a href="">Projects</a>
+                          <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+                        </div>
+                        <div className="group">
+                            <a href="">Projects</a>
+                            <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+                        </div>
+                        <div className="group">
+                                    <a href="">Projects</a>
+                                    <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+                        </div>
+                        <div className="group">
+                            <a href="">Projects</a>
+                            <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+                        </div>
+                    </div>
+                    {/* hamburger button */}
                     </nav>
+                    {/* to do mobile menu */}
+                    
                 </div>
             </section>
+            
         </div>
     );
     
