8012d410e0d49db5895609dc8e5079ada0947049
Abhinav Pal <meabhi1222@gmail.com>
2025-11-02 11:54:40 +0530

    background black

--- my-react-app/src/components/Hero.jsx
+++ my-react-app/src/components/Hero.jsx
@@ -21,7 +21,7 @@ export default function Hero() {
 
   return (
     <div>
-        <section id = "hero" >
+        <section id = "hero"  >
                 {/* max-w-6xl:widht according to screen /mx-auto:allign the whole container to center */}
                 {/* h-screen:-it makes that element as tall as the entire visible browser window. */}
                 {/* i used backgorund-size:cover; in css to cover whole container */}
